96e28d7e467268eb4399c7ce3fedd5d460f19143
Andrew Peterson <andrewpeterson1968@gmail.com>
2023-09-07 19:28:26 -0600

    Ending video 4

--- FunFile.js
+++
@@ -1,5 +0,0 @@
-import fs from "fs";
-
-if(!fs.existsSync("db.json")) {
-    console.log("File does not exist");
-}
--- queryDB.js
+++ queryDB.js
@@ -1,12 +1,33 @@
 import fs from "fs";
-import { exit } from "process";
+
 
 export default async function queryDB(externalFunction) {
-    fs.readFile("dbFileCheck.js", function(err, data){
-        if (err) {
-            console.log("Something went wrong", err);
-            exit(1)
+    try {
+        let info = [];
+
+        if (fs.existsSync("db.json")) {
+            await fs.readFile("db.json", function(error, data) {
+                if(error){
+                    console.log("Reading File Failed", error);
+                    return;
+                }
+
+                 info = JSON.parse(data.toString());
+
+                 console.log(info);
+
+                 if (externalFunction && !error) {
+                    externalFunction(info);
+                    return;
+                 }
+            });
+        } else {
+            if (externalFunction) {
+                externalFunction(info);
+                    return;
+            }
         }
-        console.log(data.toString());
-    })
+    } catch (error) {
+        console.log("Something went wrong", error);
+    }
 }
